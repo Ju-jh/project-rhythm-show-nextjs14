773fcd83ae183dbd127cf5bea4b79b0ee8e24a64
Ju-jh <jjdong9108@gmail.com>
2024-07-11 15:22:10 +0900

    fix : rhythm 스펠링 고침

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
 }>) {
   return (
     <main className="flex w-screen h-screen flex-col items-center justify-between p-24 ">
-      Rhydmn show
+      Rhythm show
       {children}
     </main>
   );
